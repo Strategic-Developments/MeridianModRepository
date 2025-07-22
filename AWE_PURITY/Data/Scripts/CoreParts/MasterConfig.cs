78194e8542ff528268a446bd6f5533bc3506791e
RELIKT77 <43426243+RELIKT77@users.noreply.github.com>
2025-07-21 21:20:54 -0700

    AWEPurity

---
+++ AWE_PURITY/Data/Scripts/CoreParts/MasterConfig.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            // naming convention: WeaponDefinition Name
+            //
+            // Enable your definitions using the follow syntax:
+            // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
+            // PartDefinitions includes both weapons and phantoms
+            PartDefinitions(
+			
+            AryxPDRailgunTurret,
+            AryxRailgun,
+            AryxGaussTurret,
+            AryxGaussCannon,
+            AryxRailgunTurret,
+            AryxRadar,
+            AryxEyeballLaser,
+            AryxHeavyCoilgun,
+            AryxLightCoilgun
+            );
+
+
+        }
+    }
+}
