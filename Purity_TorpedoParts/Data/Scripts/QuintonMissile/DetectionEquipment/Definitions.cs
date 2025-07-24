ea84b9dc1d281330b61637c7ad72e5efb882db36
Nerd-eee <nerd1234ee45e@gmail.com>
2025-07-23 19:04:12 -0700

    fix quintionsensor

--- Purity_TorpedoParts/Data/Scripts/QuintonMissile/DetectionEquipment/Definitions.cs
+++ Purity_TorpedoParts/Data/Scripts/QuintonMissile/DetectionEquipment/Definitions.cs
@@ -42,7 +42,7 @@ namespace DetectionEquipment
             RadarProperties = new SensorDefinition.RadarPropertiesDefinition
             {
                 ReceiverArea = 0.25,
-                PowerEfficiencyModifier = 2.5E-16 * 3.0938493E+05,
+                PowerEfficiencyModifier = 2.5E-16 * 4.9501589E+06,
                 Bandwidth = 1.67E6,
                 Frequency = 2800E6,
             }
