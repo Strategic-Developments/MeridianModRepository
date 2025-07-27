9d18bafce78eb4d0443e544be0c04feea80f10c0
Brillcrafter <110330962+Brillcrafter@users.noreply.github.com>
2025-07-27 16:57:16 +0100

    mod created, work started

---
+++ Brill G forces Mod/Data/Scripts/Gforces/CockpitLogicComp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+
+namespace YourName.ModName.Data.Scripts.Gforces
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Cockpit),false)]
+    public class CockpitLogicComp : MyGameLogicComponent
+    {
+        public const string ModName = "Gforces";
+        
+        private IMyCockpit _cockpit;
+        
+        private List<Vector3> _accelList = new List<Vector3>();
+
+        private float GLocTimer = 0;
+
+        private const float BlackoutAcell = 8f;
+        private const float TunnelVisionAcell = 4f;
+
+        //ok, what do I need to do
+        /* I need to check the current acceleration of the grid
+         if its above a threshold, then the pilot will start seeing a vingette on their screen
+         if it gets above a higher threshold, the pilot can black out and any inputs to the grid are maintained.
+         but having drugs in the cockpit will allow the pilot to reach much higher G forces before the effects start
+         
+         
+         
+         */
+        
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            MyLog.Default.WriteLineAndConsole(ModName + $" JumpGameLogicComponent for block {Entity.DisplayName} ({Entity.EntityId})");
+            try { throw new InvalidOperationException("break my point"); } catch(Exception) {} //debugging trigger
+            _cockpit = (IMyCockpit)Entity;
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            
+        }
+        
+        public override void UpdateBeforeSimulation()
+        {
+            var test = _cockpit.CubeGrid.Physics.LinearAcceleration;
+            _accelList.Add(test);
+            if (_accelList.Count >= 60) _accelList.RemoveAt(0);
+            if (!_cockpit.IsOccupied || !_cockpit.ControllerInfo.IsLocallyHumanControlled()) return;
+            //calculate average
+            var sum = _accelList.Sum(x => x.Length());
+            var avg = sum / _accelList.Count;
+            if (avg > TunnelVisionAcell)
+            {
+                //then start adding to the Timer
+                var mult = (avg - TunnelVisionAcell) + 1;//so it stays above 1
+                GLocTimer += mult;
+            }
+            else if (avg < BlackoutAcell && GLocTimer > 0)
+            {
+                //then they are below the threshold, 
+            }
+
+            if (GLocTimer > 3600)
+            {
+                //then you are blacked out
+                
+            }
+            
+        }
+        
+        
+    }
+}
---
+++ Brill G forces Mod/Data/Scripts/Gforces/CockpitSessionComp.cs
@@ -0,0 +1,46 @@
+﻿using VRage.Game.Components;
+using Draygo.API;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Utils;
+using VRageMath;
+
+namespace YourName.ModName.Data.Scripts.Gforces
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+    public class CockpitSessionComp : MySessionComponentBase
+    {
+        
+        //vingette texture = GLocVignette
+        
+        public HudAPIv2 HudAPI;
+
+        public bool FirstRun = true;
+
+        public HudAPIv2.BillBoardHUDMessage Vignette;
+        
+        public static bool IsDedicatedServer =>
+            MyAPIGateway.Multiplayer.MultiplayerActive && MyAPIGateway.Utilities.IsDedicated;
+
+        public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+        {
+            HudAPI = new HudAPIv2();
+            if (HudAPI == null)
+                MyAPIGateway.Utilities.ShowMessage("G forces Mod", "TextHudApi failed to register");
+        }
+
+        public override void UpdateBeforeSimulation()
+        {
+            if (HudAPI.Heartbeat && FirstRun)
+            {
+                var vignetteTexture = MyStringId.GetOrCompute("GLocVignette");
+                Vignette = new HudAPIv2.BillBoardHUDMessage(vignetteTexture, Vector2D.Zero, Color.Transparent,
+                    Shadowing: false)
+                {
+                    Visible = false
+                };
+                FirstRun = false;
+            }
+        }
+    }
+}
